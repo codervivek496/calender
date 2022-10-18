c4a74878199054afb1a2728367528d16d494a586
Vivek Kumar <kumarviv@vertafore.com>
2022-10-18 10:01:43 +0530

    canander login

---
+++ Calender/Getproject.aspx.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calender
+{
+    public partial class Getproject1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<Getproject2> list = new List<Getproject2>();
+            SqlConnection con = new SqlConnection(@"Server=HYDSQLDMO01\TRN01; Database=WI_Training_VivekKumar; Integrated Security=true");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select projectname from projectdetails", con);
+            SqlDataReader dataReader = cmd.ExecuteReader();
+            while (dataReader.Read())
+            {
+                list.Add(new Getproject2()
+                {
+                    PROJECTNAME = Convert.ToString(dataReader["projectname"])
+                });
+            }
+
+            string strproject = JsonConvert.SerializeObject(list);
+            Response.Write(strproject);
+            con.Close();
+
+
+
+        }
+    }
+}
---
+++ Calender/InsertDetails.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calender
+{
+    public partial class Infopage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Server=HYDSQLDMO01\TRN01; Database=WI_Training_VivekKumar; Integrated Security=true");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SP_XX_PROJECTDETAILS", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlParameter pKey1 = new SqlParameter("@aEMPID", Request.Form["EMPID"]);
+            SqlParameter pKey2 = new SqlParameter("@aLOGINTIME", Request.Form["LOGINTIME"]);
+            SqlParameter pKey3 = new SqlParameter("@aLOGOUTTIME", Request.Form["LOGOUTTIME"]);
+            SqlParameter pKey4 = new SqlParameter("@aPROJECTNAME", Request.Form["PROJECTNAME"]);
+            SqlParameter pKey5 = new SqlParameter("@aDESCRIPTION", Request.Form["DESCRIPTION"]);
+            cmd.Parameters.Add(pKey1);
+            cmd.Parameters.Add(pKey2);
+            cmd.Parameters.Add(pKey3);
+            cmd.Parameters.Add(pKey4);
+            cmd.Parameters.Add(pKey5);
+
+            string str = cmd.ExecuteNonQuery().ToString();
+            con.Close();
+            
+            Response.Write(str);
+        }
+    }
+}
